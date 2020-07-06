fc106f8595dd36077f0aff937e273bcdcbc5117e
paul <soapmactavish799@gmail.com>
2020-07-06 14:23:18 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 django_docker/manage.py migrate --no-input
 python3 django_docker/manage.py collectstatic --no-input
-python3 django_docker/manage.py runserver
+uwsgi --http-socket 127.0.0.1:3031 --chdir ./django_docker/ --wsgi-file django_docker/wsgi.py --master --processes 4 --threads 2 --stats 127.0.0.1:9191
