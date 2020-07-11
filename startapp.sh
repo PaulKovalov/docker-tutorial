bcd33d19f3c2cde4462e5ed141881a8ded13c891
paul <soapmactavish799@gmail.com>
2020-07-11 16:37:52 +0300

    Update run script

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,3 @@
-docker run docker_tutorial_postgres:latest --name postgres --env-file vars.env --expose 5432
+docker run docker_tutorial_postgres:latest --name postgres --env-file ./vars.env --expose 5432
 docker run docker_tutorial:latest --name app --expose 3031
 docker run docker_tutorial_nginx:latest --name nginx --expose 80
